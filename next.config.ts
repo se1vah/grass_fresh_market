9eb9aa3d3f9e1145fc177eb6b20ba68930d4b0bd
siva <sivaprakash3536@gmail.com>
2026-09-02 00:08:01 +0530

    cors fixs

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,33 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: ".next_app",
+
   async headers() {
     return [
       {
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://localhost:64188",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Authorization, Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+          },
         ],
       },
     ];
   },
-
 };
 
 export default nextConfig;
